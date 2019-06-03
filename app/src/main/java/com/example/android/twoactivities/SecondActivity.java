d3e8e49aa48967cfd668724ae012ee80b8493295
Eukris Songahid <songahid-e@cyscorpions.com>
2019-06-03 12:01:22 +0800

    Final commit

---
+++ app/src/main/java/com/example/android/twoactivities/SecondActivity.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.twoactivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * SecondActivity defines the second activity in the app. It is
+ * launched from an intent with a message, and sends an intent
+ * back with a second message.
+ */
+public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY =
+            "com.example.android.twoactivities.extra.REPLY";
+
+    private EditText mReply;
+
+    /**
+     * Initializes the activity.
+     *
+     * @param savedInstanceState The current state data
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        mReply = findViewById(R.id.editText_second);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+    }
+
+    /**
+     * Handles the onClick for the "Reply" button. Gets the message from the
+     * second EditText, creates an intent, and returns that message back to
+     * the main activity.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void returnReply(View view) {
+        String reply = mReply.getText().toString();
+
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+    }
+}
